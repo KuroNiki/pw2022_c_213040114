75e12b60b92f8844c48e6a7f3c222adb75641704
KuroNiki <cukkhotek@gmail.com>
2022-03-26 20:39:26 +0700

    menyelesaikan latihan 2

--- Kuliah/pertemuan 2/latihan1.php
+++ Kuliah/pertemuan 2/latihan1.php
@@ -1,12 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+// Pertemuan 2, belajar mengenai sintaks PHP
+// echo untuk mencetak nilai ke layar
+echo (1 + 2) * 3 / 4;
+echo "<hr>";
+
+// OPERATOR
+// Aritmatika
+// +, -, *, /, %
+echo 5 % 2; // sisa bagi
+echo "<hr>";
+echo 10 + "10" + 10;
+echo "<hr>";
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+echo 10 != 20;
+echo "<hr>";
+
+// Variabel
+// Tempat menampung nilai
+// tidak boleh mengandung spasi
+// boleh mengandung angka, tidak boleh di awal
+// snake_case : $nama_depan_mahasiswa
+// camelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+$nama = 'sandhika';
+echo $nama;
+echo "<hr>";
+$x = 1;
+$y = 2;
+$z = $x + $y;
+echo $z;
+echo "<hr>";
+
+
+// Penugasan / Assignment
+// =, +=, -=, *=, /=, %=
+$a = 10;
+$a += 20;
+$a += 30;
+echo $a;
+echo "<hr>";
+
+// Increment & Decrement
+// ++, --
+$b = 10;
+echo ++$b;
+echo "<br>";
+echo $b--;
+echo "<hr>";
+
+// Identitas
+// ===, !==
+echo 10 === "10";
+echo "<hr>";
+
+// Komponen sintax PHP yang lain
+echo "selesai mengerjakan tugas pertemuan 2";
+
+
+?>
